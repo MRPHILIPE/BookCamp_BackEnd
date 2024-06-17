7b25e0122b1c86a15dab29111ca6f6953d076d56
MRPHILIPE <mrphilipessien@gmail.com>
2024-06-17 18:23:57 +0200

    Migrations

---
+++ Migrations/20240616103635_AddPriceToCamp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bookcamp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceToCamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Camps",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Camps");
+        }
+    }
+}
