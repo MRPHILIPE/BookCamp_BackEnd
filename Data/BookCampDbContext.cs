166fc34acdb5167dc1730cd5921c34b3a1cd6322
MRPHILIPE <mrphilipessien@gmail.com>
2024-06-17 18:23:38 +0200

    Data

---
+++ Data/BookCampDbContext.cs
@@ -0,0 +1,16 @@
+﻿using bookcamp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bookcamp.Data
+{
+    public class BookCampDbContext : DbContext
+    {
+        public BookCampDbContext(DbContextOptions<BookCampDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Camp> Camps { get; set; }
+    }
+}
