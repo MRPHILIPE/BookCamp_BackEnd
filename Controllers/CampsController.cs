8b4220ef2e4d72f928603db1671b8499a91ae295
MRPHILIPE <mrphilipessien@gmail.com>
2024-06-17 18:23:05 +0200

    Controllers

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,136 @@
+﻿using bookcamp.Data;
+using bookcamp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace bookcamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampsController : ControllerBase
+    {
+        private readonly BookCampDbContext dbContext;
+        public CampsController(BookCampDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("GetCamps")]
+        public IActionResult GetCamps()
+        {
+            return Ok(dbContext.Camps.ToList());
+        }
+
+        [HttpGet]
+        [Route("GetCamp/{id}")]
+        public IActionResult GetCamp(int id)
+        {
+            var camp = dbContext.Camps.FirstOrDefault(x => x.CampId == id);
+            if (camp == null)
+            {
+                return NotFound("Camp not found");
+            }
+            return Ok(camp);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("CreateCamp")]
+        public IActionResult CreateCamp(CampDTO campDTO)
+        {
+            var userEmail = User.FindFirst(JwtRegisteredClaimNames.Email)?.Value;
+            var camp = new Camp
+            {
+                Name = campDTO.Name,
+                Location = campDTO.Location,
+                Description = campDTO.Description,
+                Price = campDTO.Price,
+                CreatedByEmail = userEmail
+            };
+            dbContext.Camps.Add(camp);
+            dbContext.SaveChanges();
+            return Ok("Camp created successfully");
+        }
+
+        [Authorize]
+        [HttpPut]
+        [Route("EditCamp/{id}")]
+        public IActionResult EditCamp(int id, CampDTO campDTO)
+        {
+            var userEmail = User.FindFirst(JwtRegisteredClaimNames.Email)?.Value;
+            var camp = dbContext.Camps.FirstOrDefault(x => x.CampId == id && x.CreatedByEmail == userEmail);
+            if (camp == null)
+            {
+                return NotFound("Camp not found or you do not have permission to edit this camp");
+            }
+
+            camp.Name = campDTO.Name;
+            camp.Location = campDTO.Location;
+            camp.Description = campDTO.Description;
+            camp.Price = campDTO.Price;
+            dbContext.SaveChanges();
+            return Ok("Camp updated successfully");
+        }
+
+        [Authorize]
+        [HttpDelete]
+        [Route("DeleteCamp/{id}")]
+        public IActionResult DeleteCamp(int id)
+        {
+            var userEmail = User.FindFirst(JwtRegisteredClaimNames.Email)?.Value;
+            var camp = dbContext.Camps.FirstOrDefault(x => x.CampId == id && x.CreatedByEmail == userEmail);
+            if (camp == null)
+            {
+                return NotFound("Camp not found or you do not have permission to delete this camp");
+            }
+
+            dbContext.Camps.Remove(camp);
+            dbContext.SaveChanges();
+            return Ok("Camp deleted successfully");
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("BookCamp/{id}")]
+        public IActionResult BookCamp(int id)
+        {
+            var userEmail = User.FindFirst(JwtRegisteredClaimNames.Email)?.Value;
+            var camp = dbContext.Camps.FirstOrDefault(x => x.CampId == id);
+            if (camp == null)
+            {
+                return NotFound("Camp not found");
+            }
+
+            if (camp.IsBooked)
+            {
+                return BadRequest("Camp is already booked");
+            }
+
+            camp.IsBooked = true;
+            dbContext.SaveChanges();
+            return Ok("Camp booked successfully");
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("UnbookCamp/{id}")]
+        public IActionResult UnbookCamp(int id)
+        {
+            var userEmail = User.FindFirst(JwtRegisteredClaimNames.Email)?.Value;
+            var camp = dbContext.Camps.FirstOrDefault(x => x.CampId == id && x.IsBooked && x.CreatedByEmail == userEmail);
+            if (camp == null)
+            {
+                return NotFound("Camp not found, not booked, or you do not have permission to unbook this camp");
+            }
+
+            camp.IsBooked = false;
+            dbContext.SaveChanges();
+            return Ok("Camp unbooked successfully");
+        }
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,180 @@
+﻿using bookcamp.Data;
+using bookcamp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace bookcamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly BookCampDbContext dbContext;
+        private readonly IConfiguration configuration;
+        public UsersController(BookCampDbContext dbContext, IConfiguration configuration)
+        {
+            this.dbContext = dbContext;
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("Registration")]
+        public IActionResult Registration(UserDTO userDTO)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userObj = dbContext.Users.FirstOrDefault(x => x.Email == userDTO.Email);
+            if (userObj == null)
+            {
+                dbContext.Users.Add(new User
+                {
+                    FirstName = userDTO.FirstName,
+                    LastName = userDTO.LastName,
+                    UserName = userDTO.UserName,
+                    Email = userDTO.Email,
+                    Password = userDTO.Password
+                });
+                dbContext.SaveChanges();
+                return Ok("User Registered Successfully");
+            }
+            else
+            {
+                return BadRequest("User already exists with the same UserName");
+            }
+
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(LoginDTO loginDTO)
+        {
+            var user = dbContext.Users.FirstOrDefault(x => x.Email == loginDTO.Email && x.Password == loginDTO.Password);
+            if (user != null)
+            {
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, configuration["Jwt:Subject"]),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim("UserId", user.UserId.ToString()),
+                    new Claim("UserName", user.UserName),
+                    new Claim("Email", user.Email),
+                    new Claim(JwtRegisteredClaimNames.Email, user.Email)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+                var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                    configuration["Jwt:Issuer"],
+                    configuration["Jwt:Audience"],
+                    claims,
+                    expires: DateTime.UtcNow.AddMinutes(30),
+                    signingCredentials: signIn
+                    );
+                string tokenvalue = new JwtSecurityTokenHandler().WriteToken(token);
+                return Ok(new { Token = tokenvalue, User = user });
+            }
+            else
+            {
+                return NoContent();
+            }
+
+        }
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public IActionResult GetUsers()
+        {
+            return Ok(dbContext.Users.ToList());
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("GetUser")]
+        public IActionResult GetUser()
+        {
+            var userId = int.Parse(User.FindFirst("UserId").Value);
+            var user = dbContext.Users.FirstOrDefault(x => x.UserId == userId);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return NotFound("User not found");
+            }
+        }
+
+        [Authorize]
+        [HttpPut]
+        [Route("EditUser")]
+        public IActionResult EditUser(EditUserDTO editUserDTO)
+        {
+            var userId = int.Parse(User.FindFirst("UserId").Value);
+            var user = dbContext.Users.FirstOrDefault(x => x.UserId == userId);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            user.FirstName = editUserDTO.FirstName;
+            user.LastName = editUserDTO.LastName;
+            user.UserName = editUserDTO.UserName;
+            user.Email = editUserDTO.Email;
+            dbContext.SaveChanges();
+
+            return Ok("User details updated successfully");
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("ChangePassword")]
+        public IActionResult ChangePassword(ChangePasswordDTO changePasswordDTO)
+        {
+            var userId = int.Parse(User.FindFirst("UserId").Value);
+            var user = dbContext.Users.FirstOrDefault(x => x.UserId == userId);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            if (user.Password != changePasswordDTO.CurrentPassword)
+            {
+                return BadRequest("Incorrect current password");
+            }
+
+            user.Password = changePasswordDTO.NewPassword;
+            dbContext.SaveChanges();
+
+            return Ok("Password changed successfully");
+        }
+
+        [Authorize]
+        [HttpDelete]
+        [Route("DeleteUser")]
+        public IActionResult DeleteUser()
+        {
+            var userId = int.Parse(User.FindFirst("UserId").Value);
+            var user = dbContext.Users.FirstOrDefault(x => x.UserId == userId);
+            if (user != null)
+            {
+                dbContext.Users.Remove(user);
+                dbContext.SaveChanges();
+                return Ok("User deleted successfully");
+            }
+            else
+            {
+                return NotFound("User not found");
+            }
+        }
+    }
+}
